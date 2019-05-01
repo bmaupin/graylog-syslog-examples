53ade93606f47e0dfe5e794e2a47e917759a9f3b
bmaupin <bmaupin@users.noreply.github.com>
2019-05-01 11:25:52 -0400

    Add test logging with stack traces

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,39 @@ public class App {
     static Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
-        logger.debug("Hello World");
+        testLoggingWithStackTraces();
+    }
+
+    private static void testLoggingWithStackTraces() {
+        makeStackTraceLonger();
+    }
+
+    private static void makeStackTraceLonger() {
+        notLongEnough();
+    }
+
+    private static void notLongEnough() {
+        okayThatsEnough();
+    }
+
+    private static void okayThatsEnough() {
+        logErrorWithStackTrace();
+        logInfoWithStackTrace();
+        logDebugWithStackTrace();
+    }
+
+    private static void logErrorWithStackTrace() {
+        logger.error("Test error message, without stack trace");
+        logger.error("Test error message, with stack trace", new IllegalArgumentException("Test exception message"));
+    }
+
+    private static void logInfoWithStackTrace() {
+        logger.info("Test info message, without stack trace");
+        logger.info("Test info message, with stack trace", new IllegalArgumentException("Test exception message"));
+    }
+
+    private static void logDebugWithStackTrace() {
+        logger.debug("Test debug message, without stack trace");
+        logger.debug("Test debug message, with stack trace", new IllegalArgumentException("Test exception message"));
     }
 }
