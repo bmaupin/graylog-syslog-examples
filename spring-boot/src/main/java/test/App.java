d1769a0c7b2b91f20dd8cd9c4f35214dfb102b67
bmaupin <bmaupin@users.noreply.github.com>
2019-08-22 15:21:11 -0400

    spring-boot: Add basic logback syslog configuration
    and some test logs with stack traces

--- spring-boot/src/main/java/test/App.java
+++ spring-boot/src/main/java/test/App.java
@@ -1,11 +1,48 @@
 package test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        testLoggingWithStackTraces();
+    }
+
+    private static void testLoggingWithStackTraces() {
+        makeStackTraceLonger();
+    }
+
+    private static void makeStackTraceLonger() {
+        notLongEnough();
+    }
+
+    private static void notLongEnough() {
+        okayThatsEnough();
+    }
+
+    private static void okayThatsEnough() {
+        logErrorWithStackTrace();
+        logInfoWithStackTrace();
+        logDebugWithStackTrace();
+    }
+
+    private static void logErrorWithStackTrace() {
+        logger.error("Test error message, without stack trace");
+        logger.error("Test error message, with stack trace", new IllegalArgumentException("Test exception message"));
+    }
+
+    private static void logInfoWithStackTrace() {
+        logger.info("Test info message, without stack trace");
+        logger.info("Test info message, with stack trace", new IllegalArgumentException("Test exception message"));
+    }
+
+    private static void logDebugWithStackTrace() {
+        logger.debug("Test debug message, without stack trace");
+        logger.debug("Test debug message, with stack trace", new IllegalArgumentException("Test exception message"));
     }
 }
